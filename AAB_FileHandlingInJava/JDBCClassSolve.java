43176047a8a74d91bf89b98ffcadc0424952b991
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-02-06 01:10:04 +0530

    n

--- AAB_FileHandlingInJava/JDBCClassSolve.java
+++ AAB_FileHandlingInJava/JDBCClassSolve.java
@@ -12,12 +12,23 @@ public class JDBCClassSolve {
             Connection connection = DriverManager.getConnection
             ("jdbc:mysql://localhost:3306/varun", "Root", "Foot");
             Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE EMP(ID INT(10) ,NAME VARCHAR(30), AGE INT )");
+            connection.commit();
             ResultSet rs = statement.executeQuery("SELECT * FROM EMP");
+            ResultSet under18 = statement.executeQuery("SELECT * FROM EMP WHERE AGE <= 18");
             while(rs.next()){
                 System.out.println
-    (rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
-                connection.close();
+    (rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
             }
+
+            System.out.println("UNDER 18");
+            while (under18.next()) {
+                System.out.println(
+                    under18.getString(1)+" "+under18.getString(2)+" "+under18.getInt(3) 
+                );
+            }
+
+            connection.close();
         }
         catch(Exception e){
             System.out.println(e);
---
+++ AAB_FileHandlingInJava/UpdateUsernamePassword.java
@@ -0,0 +1,39 @@
+package AAB_FileHandlingInJava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateUsernamePassword {
+    public static void main(String[] args) throws SQLException , ClassNotFoundException{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/varun",
+                "tomy",
+                "po983dd[]+-"
+            );    
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE USERS(USERNAME VARCHAR(15) , PASSWORD VARCHAR(15))");
+
+            statement.executeUpdate
+            ("UPDATE USERS SET PASSWORD = "+"'99898'"+" WHERE USERNAME = 'VARUN099'");
+
+            connection.commit();
+
+            statement.executeUpdate("INSERT INTO USERS VALUES('TOMMY','209R39923')");
+            connection.commit();
+
+            statement.executeUpdate("DELETE FROM USERS WHERE USERNAME ='JERRY' ");
+            connection.commit();
+
+            ResultSet allUsers = statement.executeQuery("SELECT * FROM USERS");
+
+            while (allUsers.next()) {
+                System.out.println(allUsers.getString(1)+"  "+allUsers.getString(2));
+            }
+
+            connection.close();
+    }
+}
